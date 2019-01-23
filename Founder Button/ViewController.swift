609bda1457f86dd20a1131855afb2eeb95fb80e9
Sarah Kim <sarah.mjk@hotmail.com>
2019-01-23 17:36:16 -0500

    Initial Commit

---
+++ Founder Button/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Founder Button
+//
+//  Created by Sarah Minji Kim on 1/23/19.
+//  Copyright © 2019 Sarah Minji Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
