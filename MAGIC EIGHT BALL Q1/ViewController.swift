cbf8632ab0465242f90dafd62fb8115f8903aa91
Sarah Kim <sarah.mjk@hotmail.com>
2019-02-04 18:47:38 -0500

    1/2 done

--- MAGIC EIGHT BALL Q1/ViewController.swift
+++ MAGIC EIGHT BALL Q1/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var index = -1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    let answerArray = ["It is certain",
+                       "It is decidedly so",
+                       "Without a doubt",
+                       "Yes, definitely",
+                       "You may rely on it",
+                       "As I see it, yes",
+                       "Most likely",
+                       "Outlook good",
+                       "Yes",
+                       "Signs point to yes",
+                       "Reply hazy try again",
+                       "Ask again later",
+                       "Better not tell you now",
+                       "Cannot predict now",
+                       "Concentrate and ask again",
+                       "Don't count on it",
+                       "My reply is no",
+                       "My sources say no",
+                       "Outlook not so good",
+                       "Very doubtful"]
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        //        var newPhrase = ""
+        //        repeat{
+        //            newPhrase = answerArray.randomElement()!
+        //        } while resultLabel.text == newPhrase
+        
+        //OR
+        
+        //        var newPhrase = answerArray.randomElement()!
+        //        while resultLabel.text == newPhrase{
+        //            newPhrase = answerArray.randomElement()!
+        //        }
+        //        resultLabel.text = newPhrase
+        
+        //OR
+        
+        var newIndex = Int.random(in: 0..<answerArray.count)
+        while index == newIndex {
+            newIndex = Int.random(in: 0..<answerArray.count)
+        }
+        index = newIndex
+        resultLabel.text = answerArray[index]
+    }
+    
 }
-
